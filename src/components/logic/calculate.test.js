00cb9cfb55aa5cdca55dc9c904729d9fa0f22f4f
diarisdiakite <diarisdiakite@gmail.com>
2023-08-16 01:55:54 +0200

    Add tests for calculate function.

---
+++ src/components/logic/calculate.test.js
@@ -0,0 +1,28 @@
+import calculate from "./calculate";
+
+/* 
+  function calculate(obj, buttonName)
+  takes 2 params
+  
+  example obj === {
+    total: '0',
+    next: '5',
+    operation: '-'
+  }
+  buttonName === keypad buttons
+*/
+
+describe('Test calculate function', () => {
+  test('Should update the number to displayed "52"', () => {
+    const result = calculate({total:'10', next:'5', operation:'+'}, '2')
+    expect(result).toEqual({total:'10', next:'52', operation:'+'})
+  });
+  test('Should update the result object with the values of calculate and the operator to "-"', () => {
+    const result = calculate({total:'10', next:'52', operation:'+'}, '-')
+    expect(result).toEqual({total:'62', next: null, operation:'-'})
+  });
+  test('Should reinitialize total and next to null', () => {
+    const result = calculate({total:'62', next: null, operation: '-'}, 'AC')
+    expect(result).toEqual({total: null, next: null, operation: null})
+  })
+})
