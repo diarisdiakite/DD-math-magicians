c517744e7c1b71cd5f8a8051000bf0f3cb0b42ed
diarisdiakite <diarisdiakite@gmail.com>
2023-08-16 22:15:05 +0200

    Add React Testing Library and simulate user interaction on the Header component.

--- src/components/pagesComponents/Header.test.js
+++ src/components/pagesComponents/Header.test.js
@@ -1,5 +1,7 @@
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Header from './Header';
 
 describe('Header component', () => {
@@ -12,5 +14,21 @@ describe('Header component', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('Cliking on a links should open the page it links to', () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
+    const homeNavLink = screen.getByRole('link', { name: 'Home' });
+    describe('Should led the user to the Home page when he clicks Home navelink', () => {
+      expect(homeNavLink).toBeDefined();
+
+      const expectedPath = '/';
+      userEvent.click(homeNavLink);
+      expect(window.location.pathname).toEqual(expectedPath);
+    });
   });
 });
