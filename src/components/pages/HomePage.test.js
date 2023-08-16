216cd1d56fa2f275f5955f596b747cbd28ddd8e2
diarisdiakite <diarisdiakite@gmail.com>
2023-08-16 18:09:01 +0200

    Fix linters errors.

--- src/components/pages/HomePage.test.js
+++ src/components/pages/HomePage.test.js
@@ -1,18 +1,16 @@
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 
-
-
 describe('Header component', () => {
   it('renders correctly', () => {
-      const tree = renderer
-        .create(
+    const tree = renderer
+      .create(
         <MemoryRouter>
           <HomePage />
-        </MemoryRouter>
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
-  });  
-})
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/components/pages/QuotePage.test.js
+++ src/components/pages/QuotePage.test.js
@@ -1,18 +1,16 @@
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 import QuotePage from './QuotePage';
 
-
-
 describe('Header component', () => {
   it('renders correctly', () => {
-      const tree = renderer
-        .create(
+    const tree = renderer
+      .create(
         <MemoryRouter>
           <QuotePage />
-        </MemoryRouter>
-        )
-        .toJSON();
-      expect(tree).toMatchSnapshot();
-  });  
-})
+        </MemoryRouter>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
