bba60a67abec628c43471bf387f3eae3f9487a1b
diarisdiakite <diarisdiakite@gmail.com>
2023-08-16 05:14:13 +0200

    Add Snapshot tests for Header and Footer components.

---
+++ src/components/pagesComponents/Footer.test.js
@@ -0,0 +1,18 @@
+import renderer from 'react-test-renderer'
+import { MemoryRouter } from 'react-router-dom';
+import Footer from "./Header";
+
+
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+      const tree = renderer
+        .create(
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+        )
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+  });  
+})
